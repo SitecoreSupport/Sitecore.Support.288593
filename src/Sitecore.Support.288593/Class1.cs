d02ed53591157280e1a7aaf41b9d9c4674ea4477
DK\whc <whc@sitecore.net>
2018-10-26 15:48:56 +0800

    Initial commit: added IF statement to get the job done

---
+++ src/Sitecore.Support.288593/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.XA.Foundation.SitecoreExtensions.Pipelines.RenderField
+{
+  using Sitecore.Collections;
+  using Sitecore.Data.Fields;
+  using Sitecore.Pipelines.RenderField;
+
+  public class SetImageStyleParameters
+  {
+    public void Process(RenderFieldArgs args)
+    {
+      if (args.FieldTypeKey == "image")
+      {
+        ImageField imageField = args.GetField();
+        if (!string.IsNullOrWhiteSpace(imageField.VSpace + imageField.HSpace))// Patch 287589 reporting...
+        {
+          string arg = string.IsNullOrWhiteSpace(imageField.VSpace) ? "0" : imageField.VSpace;
+          string arg2 = string.IsNullOrWhiteSpace(imageField.HSpace) ? "0" : imageField.HSpace;
+          ((SafeDictionary<string, string>) args.Parameters)["style"] = string.Format("margin: {0}px {1}px", arg, arg2);
+        }
+      }
+    }
+  }
+}
